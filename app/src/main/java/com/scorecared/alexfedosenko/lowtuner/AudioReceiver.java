57221c7e380acff318017eaa9fce946d4e6fae08
Oleksandr Fedosenko <ofedosenko@cogniance.com>
2016-02-11 10:45:43 +0200

    Add method for the FFT

--- app/src/main/java/com/scorecared/alexfedosenko/lowtuner/AudioReceiver.java
+++ app/src/main/java/com/scorecared/alexfedosenko/lowtuner/AudioReceiver.java
@@ -3,28 +3,212 @@ package com.scorecared.alexfedosenko.lowtuner;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.util.Log;
+
+import org.apache.commons.math3.complex.Complex;
 
 public class AudioReceiver {
 
+    private static final String TAG = "AudioReceiver";
     private static final Thread mPollingAudioThread = new Thread() {
 
         @Override
         public void run() {
             android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
-            int n = AudioRecord.getMinBufferSize(10000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-            AudioRecord record = new AudioRecord(MediaRecorder.AudioSource.MIC, 10000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, 100000);
+            AudioRecord record = findAudioRecord();
             record.startRecording();
-            short[][] buffers = new short[256][160];
+            byte[][] buffers = new byte[256][1024];
             int ix = 0;
 
             do {
-                short[] buffer = buffers[ix++ % buffers.length];
-                n = record.read(buffer, 0, buffer.length);
+                byte[] buffer = buffers[ix++ % buffers.length];
+                record.read(buffer, 0, buffer.length);
+                calculateFFT(buffer);
             } while (!interrupted());
             record.stop();
             record.release();
         }
 
+        private int[] mSampleRates = new int[] { 44100, 22050, 11025, 8000 };
+        public AudioRecord findAudioRecord() {
+            for (int rate : mSampleRates) {
+                for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_16BIT, AudioFormat.ENCODING_PCM_8BIT }) {
+                    for (short channelConfig : new short[] { AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO }) {
+                        try {
+                            Log.d(TAG, "Attempting rate " + rate + "Hz, bits: " + audioFormat + ", channel: "
+                                    + channelConfig);
+                            int bufferSize = AudioRecord.getMinBufferSize(rate, channelConfig, audioFormat);
+
+                            if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
+                                // check if we can instantiate and have a success
+                                AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, rate, channelConfig, audioFormat, bufferSize);
+
+                                if (recorder.getState() == AudioRecord.STATE_INITIALIZED)
+                                    return recorder;
+                            }
+                        } catch (Exception e) {
+                            Log.e(TAG, rate + "Exception, keep trying.",e);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public double[] calculateFFT(byte[] signal)
+        {
+            final int mNumberOfFFTPoints =1024;
+            double mMaxFFTSample;
+            int mPeakPos;
+
+            double temp;
+            double[] y;
+            Complex[] complexSignal = new Complex[mNumberOfFFTPoints];
+            double[] absSignal = new double[mNumberOfFFTPoints/2];
+
+            for(int i = 0; i < mNumberOfFFTPoints; i++){
+                temp = (double)((signal[2*i] & 0xFF) | (signal[2*i+1] << 8)) / 32768.0F;
+                complexSignal[i] = new Complex(temp,0.0);
+            }
+
+            y = fft(complexSignal, true);
+//            y = FFT.fft(complexSignal); // --> Here I use FFT class
+
+//            mMaxFFTSample = 0.0;
+//            mPeakPos = 0;
+//            for(int i = 0; i < (mNumberOfFFTPoints/2); i++)
+//            {
+//                absSignal[i] = Math.sqrt(Math.pow(y[i].getReal(), 2) + Math.pow(y[i].getImaginary(), 2));
+//                if(absSignal[i] > mMaxFFTSample)
+//                {
+//                    mMaxFFTSample = absSignal[i];
+//                    mPeakPos = i;
+//                }
+//            }
+
+            return y;
+
+        }
+        /**
+         * The Fast Fourier Transform (generic version, with NO optimizations).
+         *
+         * @param inputComplex
+         *            an array of length n of complex values
+         * @param DIRECT
+         *            TRUE = direct transform, FALSE = inverse transform
+         * @return a new array of length 2n
+         */
+        public double[] fft(final Complex[] inputComplex,
+                                   boolean DIRECT) {
+            // - n is the dimension of the problem
+            // - nu is its logarithm in base e
+            int n = inputComplex.length;
+
+            // If n is a power of 2, then ld is an integer (_without_ decimals)
+            double ld = Math.log(n) / Math.log(2.0);
+
+            // Here I check if n is a power of 2. If exist decimals in ld, I quit
+            // from the function returning null.
+            if (((int) ld) - ld != 0) {
+                System.out.println("The number of elements is not a power of 2.");
+                return null;
+            }
+
+            // Declaration and initialization of the variables
+            // ld should be an integer, actually, so I don't lose any information in
+            // the cast
+            int nu = (int) ld;
+            int n2 = n / 2;
+            int nu1 = nu - 1;
+            double[] xReal = new double[n];
+            double[] xImag = new double[n];
+            double tReal, tImag, p, arg, c, s;
+
+            // Here I check if I'm going to do the direct transform or the inverse
+            // transform.
+            double constant;
+            if (DIRECT)
+                constant = -2 * Math.PI;
+            else
+                constant = 2 * Math.PI;
+
+            // I don't want to overwrite the input arrays, so here I copy them. This
+            // choice adds \Theta(2n) to the complexity.
+            for (int i = 0; i < n; i++) {
+                xReal[i] = inputComplex[i].getReal();
+                xImag[i] = inputComplex[i].getImaginary();
+            }
+
+            // First phase - calculation
+            int k = 0;
+            for (int l = 1; l <= nu; l++) {
+                while (k < n) {
+                    for (int i = 1; i <= n2; i++) {
+                        p = bitreverseReference(k >> nu1, nu);
+                        // direct FFT or inverse FFT
+                        arg = constant * p / n;
+                        c = Math.cos(arg);
+                        s = Math.sin(arg);
+                        tReal = xReal[k + n2] * c + xImag[k + n2] * s;
+                        tImag = xImag[k + n2] * c - xReal[k + n2] * s;
+                        xReal[k + n2] = xReal[k] - tReal;
+                        xImag[k + n2] = xImag[k] - tImag;
+                        xReal[k] += tReal;
+                        xImag[k] += tImag;
+                        k++;
+                    }
+                    k += n2;
+                }
+                k = 0;
+                nu1--;
+                n2 /= 2;
+            }
+
+            // Second phase - recombination
+            k = 0;
+            int r;
+            while (k < n) {
+                r = bitreverseReference(k, nu);
+                if (r > k) {
+                    tReal = xReal[k];
+                    tImag = xImag[k];
+                    xReal[k] = xReal[r];
+                    xImag[k] = xImag[r];
+                    xReal[r] = tReal;
+                    xImag[r] = tImag;
+                }
+                k++;
+            }
+
+            // Here I have to mix xReal and xImag to have an array (yes, it should
+            // be possible to do this stuff in the earlier parts of the code, but
+            // it's here to readibility).
+            double[] newArray = new double[xReal.length * 2];
+            double radice = 1 / Math.sqrt(n);
+            for (int i = 0; i < newArray.length; i += 2) {
+                int i2 = i / 2;
+                // I used Stephen Wolfram's Mathematica as a reference so I'm going
+                // to normalize the output while I'm copying the elements.
+                newArray[i] = xReal[i2] * radice;
+                newArray[i + 1] = xImag[i2] * radice;
+            }
+            return newArray;
+        }
+
+        /**
+         * The reference bitreverse function.
+         */
+        private int bitreverseReference(int j, int nu) {
+            int j2;
+            int j1 = j;
+            int k = 0;
+            for (int i = 1; i <= nu; i++) {
+                j2 = j1 / 2;
+                k = 2 * k + j1 - 2 * j2;
+                j1 = j2;
+            }
+            return k;
+        }
 
     };
 
--- app/src/main/java/com/scorecared/alexfedosenko/lowtuner/ui/MainActivity.java
+++ app/src/main/java/com/scorecared/alexfedosenko/lowtuner/ui/MainActivity.java
@@ -1,10 +1,15 @@
 package com.scorecared.alexfedosenko.lowtuner.ui;
 
+import android.content.Context;
+import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.scorecared.alexfedosenko.lowtuner.AudioReceiver;
 import com.scorecared.alexfedosenko.lowtuner.R;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +22,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        AudioReceiver.start();
+        if (getMicrophoneAvailable(this)) {
+            AudioReceiver.start();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         AudioReceiver.stop();
+    }
+
+    public static boolean getMicrophoneAvailable(Context context) {
+        MediaRecorder recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.VOICE_RECOGNITION);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        recorder.setOutputFile(new File(context.getCacheDir(), "MediaUtil#micAvailTestFile").getAbsolutePath());
+        boolean available = true;
+        try {
+            recorder.prepare();
+        }
+        catch (IOException exception) {
+            available = false;
+        }
+        recorder.release();
+        return available;
     }
 }
