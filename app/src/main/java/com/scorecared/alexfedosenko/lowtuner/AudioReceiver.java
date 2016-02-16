b6ffa5d9f7555ed4ac349864c218de33f2bebf7b
Oleksandr Fedosenko <ofedosenko@cogniance.com>
2016-02-16 13:30:13 +0200

    Update audio thread

--- app/src/main/java/com/scorecared/alexfedosenko/lowtuner/AudioReceiver.java
+++ app/src/main/java/com/scorecared/alexfedosenko/lowtuner/AudioReceiver.java
@@ -3,6 +3,7 @@ package com.scorecared.alexfedosenko.lowtuner;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import org.apache.commons.math3.complex.Complex;
@@ -10,29 +11,29 @@ import org.apache.commons.math3.complex.Complex;
 public class AudioReceiver {
 
     private static final String TAG = "AudioReceiver";
-    private static final Thread mPollingAudioThread = new Thread() {
-
+    private static final AsyncTask<Void, Double, Void> mAudioTask = new AsyncTask<Void, Double, Void>() {
         @Override
-        public void run() {
+        protected Void doInBackground(Void... params) {
             android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
             AudioRecord record = findAudioRecord();
             record.startRecording();
-            byte[][] buffers = new byte[256][1024];
+            byte[][] buffers = new byte[256][8192];
             int ix = 0;
 
             do {
                 byte[] buffer = buffers[ix++ % buffers.length];
                 record.read(buffer, 0, buffer.length);
                 calculateFFT(buffer);
-            } while (!interrupted());
+            } while (!isCancelled());
             record.stop();
             record.release();
+            return null;
         }
 
         private int[] mSampleRates = new int[] { 44100, 22050, 11025, 8000 };
         public AudioRecord findAudioRecord() {
             for (int rate : mSampleRates) {
-                for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_16BIT, AudioFormat.ENCODING_PCM_8BIT }) {
+                for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT }) {
                     for (short channelConfig : new short[] { AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO }) {
                         try {
                             Log.d(TAG, "Attempting rate " + rate + "Hz, bits: " + audioFormat + ", channel: "
@@ -57,37 +58,60 @@ public class AudioReceiver {
 
         public double[] calculateFFT(byte[] signal)
         {
-            final int mNumberOfFFTPoints =1024;
+            final int mNumberOfFFTPoints = 4096;
             double mMaxFFTSample;
             int mPeakPos;
 
             double temp;
             double[] y;
             Complex[] complexSignal = new Complex[mNumberOfFFTPoints];
-            double[] absSignal = new double[mNumberOfFFTPoints/2];
+            double[] absSignal = new double[mNumberOfFFTPoints / 2];
 
             for(int i = 0; i < mNumberOfFFTPoints; i++){
                 temp = (double)((signal[2*i] & 0xFF) | (signal[2*i+1] << 8)) / 32768.0F;
                 complexSignal[i] = new Complex(temp,0.0);
             }
 
-            y = fft(complexSignal, true);
+            y = gaus(fft(complexSignal, true));
 //            y = FFT.fft(complexSignal); // --> Here I use FFT class
 
-//            mMaxFFTSample = 0.0;
-//            mPeakPos = 0;
-//            for(int i = 0; i < (mNumberOfFFTPoints/2); i++)
-//            {
-//                absSignal[i] = Math.sqrt(Math.pow(y[i].getReal(), 2) + Math.pow(y[i].getImaginary(), 2));
-//                if(absSignal[i] > mMaxFFTSample)
-//                {
-//                    mMaxFFTSample = absSignal[i];
-//                    mPeakPos = i;
-//                }
-//            }
+            mMaxFFTSample = 0.0;
+            mPeakPos = 0;
+            try {
+                for (int i = 0; i < (mNumberOfFFTPoints / 2); i++) {
+                    absSignal[i] = Math.sqrt(Math.pow(y[i * 2], 2) + Math.pow(y[i * 2 + 1], 2));
+                    if (absSignal[i] > mMaxFFTSample) {
+                        mMaxFFTSample = absSignal[i];
+                        mPeakPos = i;
+                    }
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                Log.e(TAG, "End of the array", e);
+            }
+
+            double result = mPeakPos * 44100.0 / 2048;
+            if (result <= 5000) {
+                Log.w(TAG, "Frequency = " + mPeakPos * 44100.0 / mNumberOfFFTPoints);
+            }
 
             return y;
 
+        }
+
+        public double gausse(double n, double frameSize)
+        {
+            double a = (frameSize - 1)/2;
+            double t = (n - a)/(0.5*a);
+            t = t*t;
+            return Math.exp(-t/2);
+        }
+
+        private double[] gaus(double[] n) {
+            double[] result = new double[n.length];
+            for (int i = 0; i < n.length; i++) {
+                result[i] = gausse(n[i], 8192);
+            }
+            return result;
         }
         /**
          * The Fast Fourier Transform (generic version, with NO optimizations).
@@ -99,7 +123,7 @@ public class AudioReceiver {
          * @return a new array of length 2n
          */
         public double[] fft(final Complex[] inputComplex,
-                                   boolean DIRECT) {
+                            boolean DIRECT) {
             // - n is the dimension of the problem
             // - nu is its logarithm in base e
             int n = inputComplex.length;
@@ -109,6 +133,10 @@ public class AudioReceiver {
 
             // Here I check if n is a power of 2. If exist decimals in ld, I quit
             // from the function returning null.
+            if ((n & (n - 1)) != 0) {
+                System.out.println("The number of elements is not a power of 2.");
+                return null;
+            }
             if (((int) ld) - ld != 0) {
                 System.out.println("The number of elements is not a power of 2.");
                 return null;
@@ -209,14 +237,13 @@ public class AudioReceiver {
             }
             return k;
         }
-
     };
 
     public static void start() {
-        mPollingAudioThread.start();
+        mAudioTask.execute();
     }
 
     public static void stop() {
-        mPollingAudioThread.interrupt();
+        mAudioTask.cancel(true);
     }
 }
